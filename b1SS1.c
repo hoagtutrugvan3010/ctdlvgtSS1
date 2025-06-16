c4709c5ed65990565c06ae5839db531f24da2f67
hoagtutrugvan3010 <thieutiendung3010@gamil.com>
2025-06-16 09:55:51 +0700

    update file

---
+++ b1SS1.c
@@ -0,0 +1,12 @@
+//
+// Created by Admin on 6/16/2025.
+//
+#include "stdio.h"
+int main() {
+    int n;
+    for (int i = 0; i< n;i++) {
+        printf("hello\n");
+    }
+    return 0;
+}
+//vi mang duyet qua 1 vong lap chay den khi i<n -> chay n lan -> do phuc tap la o(n)
